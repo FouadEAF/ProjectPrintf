b85f33968de563ccfe1679ac49d0e94144e82291
Developper_EAF <50346738+FouadEAF@users.noreply.github.com>
2023-04-24 15:49:43 +0000

    Update main.h

--- main.h
+++ main.h
@@ -26,12 +26,12 @@ int print_usr(va_list arguments, char *buf, unsigned int ibuf);
 int print_add(va_list arguments, char *buf, unsigned int ibuf);
 int print_rev(va_list arguments, char *buf, unsigned int ibuf);
 int print_rot(va_list arguments, char *buf, unsigned int ibuf);
-int printplusint(va_list arguments, char *buf, unsigned int ibuf);
-int printspcint(va_list arguments, char *buf, unsigned int ibuf);
 int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
 int ev_print_func(const char *s, int index);
 unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
 int print_buf(char *buf, unsigned int nbuf);
+int printplusint(va_list arguments, char *buf, unsigned int ibuf);
+int printspcint(va_list arguments, char *buf, unsigned int ibuf);
 
 int printLint(va_list arguments, char *buf, unsigned int ibuf);
 int printLunint(va_list arguments, char *buf, unsigned int ibuf);
