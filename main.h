0dcd57a80abbc59e659ff6abb214315029905c40
Developper_EAF <50346738+FouadEAF@users.noreply.github.com>
2023-04-24 15:21:02 +0000

    Update main.h

--- main.h
+++ main.h
@@ -26,25 +26,29 @@ int print_usr(va_list arguments, char *buf, unsigned int ibuf);
 int print_add(va_list arguments, char *buf, unsigned int ibuf);
 int print_rev(va_list arguments, char *buf, unsigned int ibuf);
 int print_rot(va_list arguments, char *buf, unsigned int ibuf);
-int prinlint(va_list arguments, char *buf, unsigned int ibuf);
-int prinlunt(va_list arguments, char *buf, unsigned int ibuf);
-int prinloct(va_list arguments, char *buf, unsigned int ibuf);
-int prinlhex(va_list arguments, char *buf, unsigned int ibuf);
-int prinlupx(va_list arguments, char *buf, unsigned int ibuf);
-int prinhint(va_list arguments, char *buf, unsigned int ibuf);
-int prinhunt(va_list arguments, char *buf, unsigned int ibuf);
-int prinhoct(va_list arguments, char *buf, unsigned int ibuf);
-int prinhhex(va_list arguments, char *buf, unsigned int ibuf);
-int prinhupx(va_list arguments, char *buf, unsigned int ibuf);
-int prinpint(va_list arguments, char *buf, unsigned int ibuf);
-int prinnoct(va_list arguments, char *buf, unsigned int ibuf);
-int prinnhex(va_list arguments, char *buf, unsigned int ibuf);
-int prinnupx(va_list arguments, char *buf, unsigned int ibuf);
-int prinsint(va_list arguments, char *buf, unsigned int ibuf);
+int printplusint(va_list arguments, char *buf, unsigned int ibuf);
+int printspcint(va_list arguments, char *buf, unsigned int ibuf);
 int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
 int ev_print_func(const char *s, int index);
 unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
 int print_buf(char *buf, unsigned int nbuf);
+
+int printLint(va_list arguments, char *buf, unsigned int ibuf);
+int printLunint(va_list arguments, char *buf, unsigned int ibuf);
+int printLoct(va_list arguments, char *buf, unsigned int ibuf);
+int printLhex(va_list arguments, char *buf, unsigned int ibuf);
+int printLupx(va_list arguments, char *buf, unsigned int ibuf);
+
+int printShInt(va_list arguments, char *buf, unsigned int ibuf);
+int printShunInt(va_list arguments, char *buf, unsigned int ibuf);
+int printShOct(va_list arguments, char *buf, unsigned int ibuf);
+int printShHex(va_list arguments, char *buf, unsigned int ibuf);
+int printShupx(va_list arguments, char *buf, unsigned int ibuf);
+
+int printnumoct(va_list arguments, char *buf, unsigned int ibuf);
+int printnumhex(va_list arguments, char *buf, unsigned int ibuf);
+int printnumupx(va_list arguments, char *buf, unsigned int ibuf);
+
 char *fill_binary_array(char *binary, long int int_in, int isneg, int limit);
 char *fill_oct_array(char *bnr, char *oct);
 char *fill_long_oct_array(char *bnr, char *oct);
